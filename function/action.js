b8bf5060c56131fae96735a8b69d2a4564334c00
Jeya Karthika <hello@jeyakarthika.com>
2014-02-04 11:59:17 +1000

    Updated File names

--- function/action.js
+++ function/action.js
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------------
 
-	Project Name: Bare Bones
-	Project Description: A Starter Template for my Front-End Web Development Projects
+	Project Name: Eliya
+	Project Description: A Starter Template for my Front-End Projects
 	File Name: action.js
 	Author: Jeya Karthika
 	Author URI: http://www.freshsqueaks.com
